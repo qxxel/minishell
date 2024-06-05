8c60cdc2a92bb3ab9224d15d7e10cac3c4a02971
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-05 13:17:27 +0200

    Add headers

--- include/minishell.h
+++ include/minishell.h
@@ -6,13 +6,28 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:05:57 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/05 12:07:05 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/05 13:17:01 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
 # include "libft.h"
 
 #endif
