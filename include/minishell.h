fbb4bc6130307d2be09a42267aea9530f99d9992
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-06 14:22:49 +0200

    Add msh_includes.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,29 +6,13 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:05:57 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/06 14:22:17 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/06 14:22:31 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <dirent.h>
-# include <fcntl.h>
-# include <readline/history.h>
-# include <readline/readline.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/resource.h>
-# include <sys/stat.h>
-# include <sys/time.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <termios.h>
-# include <unistd.h>
-# include "libft.h"
-# include "msh_types.h"
+# include "msh_includes.h"
 
 #endif
---
+++ include/msh_includes.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   msh_includes.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 14:17:11 by deydoux           #+#    #+#             */
+/*   Updated: 2024/06/06 14:18:18 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MSH_INCLUDES_H
+# define MSH_INCLUDES_H
+
+# include <dirent.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <termios.h>
+# include <unistd.h>
+
+# include "libft.h"
+# include "msh_types.h"
+
+#endif
