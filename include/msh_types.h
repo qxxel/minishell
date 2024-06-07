c07818a0a90965b6e298449a889ca8b490ab7100
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-07 14:25:53 +0200

    Update s_cmd

--- include/msh_types.h
+++ include/msh_types.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:02:24 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/06 14:20:50 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/07 14:25:34 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@
 
 typedef struct s_cmd
 {
-	char	**argv;
-	char	*heredoc_delimiter;
-	char	*in_path;
-	char	*out_path;
-	char	*program_path;
-	int		append_out;
+	char			**argv;
+	char			*bin;
+	char			*heredoc;
+	char			*in;
+	char			*out;
+	int				append;
+	struct s_cmd	*and;
+	struct s_cmd	*cmds;
+	struct s_cmd	*or;
 }	t_cmd;
 
 #endif
