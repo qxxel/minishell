0ebea39366891a6bbed03b53bd4c0ed5d7e516e5
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-11 16:36:28 +0200

    Update type of append to bool in s_cmd

--- include/msh_types.h
+++ include/msh_types.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:02:24 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/11 16:32:53 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/11 16:33:46 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 
 typedef struct s_cmd
 {
+	bool	append;
 	char	**argv;
 	char	*heredoc;
 	char	*in;
 	char	*out;
-	int		append;
 }	t_cmd;
 
 #endif
