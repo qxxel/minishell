a119dcf2f0928ce65cc32aaa7aba2fdda3abadd2
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-11 16:32:13 +0200

    Remove and, cmds, or and pipe in s_cmd

--- include/msh_types.h
+++ include/msh_types.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:02:24 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/10 16:46:11 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/11 16:31:00 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,6 @@ typedef struct s_cmd
 	char			*in;
 	char			*out;
 	int				append;
-	struct s_cmd	*and;
-	struct s_cmd	*cmds;
-	struct s_cmd	*or;
-	struct s_cmd	*pipe;
 }	t_cmd;
 
 #endif
