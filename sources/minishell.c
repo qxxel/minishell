82f753a9418f789f55101c0eb049dce94b8e8278
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-14 20:14:05 +0200

    Fix norm in minishell.c

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/14 20:10:25 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/14 20:13:43 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ bool	msh_split(char *str, char ***strs);
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*str = "ls '-la u'\"uwu   \\\"eheheheh       \"| cat -e >> my_outfile | wc\\";
 	char	**strs;
+	char	*str;
 	size_t	i;
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	str = ft_strdup("ls '-la u'\"uwu   \\\"eheheheh       \"| cat -e >> my_outf\
+ile | wc\\");
 	if (msh_split(str, &strs))
 		return (EXIT_FAILURE);
 	i = 0;
