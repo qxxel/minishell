996630c1eb383b32ef8be86e5cac9d9f94e7c316
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-16 17:14:15 +0200

    Move tests in parse_cmds.c

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/15 17:54:31 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/16 17:14:23 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,13 @@ bool	msh_split(char *str, char ***strs);
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**strs;
 	char	*str;
-	size_t	i;
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	str = ft_strdup("echo \"Hello\"'World' '' | cat -e >> my_outfile | wc");
-	if (msh_split(str, &strs))
-		return (EXIT_FAILURE);
+	parse_cmds(str, NULL);
 	free(str);
-	i = -1;
-	while (strs[++i])
-	{
-		printf("%s\n", strs[i]);
-		free(strs[i]);
-	}
-	free(strs);
 	return (EXIT_SUCCESS);
 }
--- sources/parse_cmds/parse_cmds.c
+++ sources/parse_cmds/parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 16:38:50 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/16 17:06:24 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/16 17:08:48 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 bool	parse_cmds(char *str, t_cmd **cmds)
 {
+	size_t	i;
 	char	**strs;
 
 	if (mark_quotes(str) || msh_split(str, &strs))
 		return (true);
 	ignore_empty_quotes(strs);
+	i = -1;
+	while (strs[++i])
+	{
+		printf("%s;\n", strs[i]);
+		free(strs[i]);
+	}
+	free(strs);
 	return (false);
 	(void)cmds;
 }
