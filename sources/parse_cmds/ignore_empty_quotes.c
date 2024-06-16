edc812c6022ea0ed56185dc00ffe2b1335dff7a1
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-16 17:06:40 +0200

    Add ignore_empty_quotes

--- sources/parse_cmds/ignore_empty_quotes.c
+++ sources/parse_cmds/ignore_empty_quotes.c
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_cmds.c                                       :+:      :+:    :+:   */
+/*   ignore_empty_quotes.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/16 16:38:50 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/16 17:06:24 by deydoux          ###   ########.fr       */
+/*   Created: 2024/06/16 17:03:40 by deydoux           #+#    #+#             */
+/*   Updated: 2024/06/16 17:05:57 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse_cmds.h"
 
-bool	parse_cmds(char *str, t_cmd **cmds)
+void	ignore_empty_quotes(char **strs)
 {
-	char	**strs;
+	size_t	i;
 
-	if (mark_quotes(str) || msh_split(str, &strs))
-		return (true);
-	ignore_empty_quotes(strs);
-	return (false);
-	(void)cmds;
+	i = 0;
+	while (strs[i])
+	{
+		if (ft_strchr(QUOTES, strs[i][0]) && strs[i][1])
+			shift_strs(&strs[i]);
+		else
+			i++;
+	}
 }
