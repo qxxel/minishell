67a34ea7fdf9e65489844b9b3d9f5e83a7dd29a0
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-19 12:30:57 +0200

    Add get_env_var prototype in msh_commons.h

--- sources/minishell.h
+++ sources/minishell.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:05:57 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/18 18:01:18 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/18 18:12:06 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include "msh_commons.h"
 
-char	*get_env_var(char *name, size_t len, t_list *env);
 bool	init_env(char **initial, t_msh	*msh);
 bool	parse_cmds(char *str, t_cmd **cmds);
 
