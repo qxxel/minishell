a942cdab618a80c6322a0971c4f56f60f8871431
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-20 14:32:59 +0200

    Add copy_expand

--- sources/parse_cmds/expand_env.c
+++ sources/parse_cmds/expand_env.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:16:11 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/19 18:24:26 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/20 14:32:36 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ static size_t	get_var_len(char *str)
 	return (len);
 }
 
-static size_t	var_size(char *str, size_t *start, char **envp)
+static size_t	value_len(char *str, size_t *i, char **envp)
 {
 	char	*value;
 	size_t	var_len;
 
 	var_len = get_var_len(str);
-	*start += var_len;
+	*i += var_len;
 	if (!var_len)
 		return (1);
 	value = get_env_var(str, var_len, envp);
@@ -37,7 +37,7 @@ static size_t	var_size(char *str, size_t *start, char **envp)
 	return (ft_strlen(value));
 }
 
-static size_t	new_str_size(char *str, char **envp)
+static size_t	expand_size(char *str, char **envp)
 {
 	size_t	i;
 	size_t	size;
@@ -47,15 +47,57 @@ static size_t	new_str_size(char *str, char **envp)
 	while (str[++i])
 	{
 		if (str[i] == '$')
-			size += var_size(&str[i + 1], &i, envp);
+			size += value_len(&str[i + 1], &i, envp);
 		else
 			size++;
 	}
 	return (size);
 }
 
+static void	copy_expand(char *src, char *dst, char **envp)
+{
+	char	*value;
+	size_t	i_dst;
+	size_t	i_src;
+	size_t	var_len;
+
+	i_src = -1;
+	i_dst = 0;
+	while (src[++i_src])
+	{
+		if (src[i_src] == '$')
+		{
+			var_len = get_var_len(&src[i_src + 1]);
+			if (!var_len)
+				dst[i_dst++] = '$';
+			else
+			{
+				value = get_env_var(&src[i_src + 1], var_len, envp);
+				if (*value)
+					while (*value)
+						dst[i_dst++] = *value++;
+				i_src += var_len;
+			}
+		}
+		else
+			dst[i_dst++] = src[i_src];
+	}
+	dst[i_dst] = 0;
+}
+
 bool	expand_env(char **str, char **envp)
 {
-	printf("%zu %zu\n", ft_strlen(*str), new_str_size(*str, envp));
+	char	*expand;
+
+	expand = malloc((expand_size(*str, envp) + 1) * sizeof(char));
+	if (!expand)
+	{
+		perror("malloc");
+		return (true);
+	}
+	copy_expand(*str, expand, envp);
+	printf("EXPAND_ENV:\n%s\n%s\n", *str, expand);
+	free(*str);
+	*str = expand;
 	return (false);
 }
