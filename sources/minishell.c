e606f76b7a684435f77b5cd0600f1e4647289ff6
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-20 16:32:09 +0200

    Use destroy_msh

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/19 17:49:18 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/20 16:29:30 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,9 @@ int	main(int argc, char **argv, char **envp)
 	ft_bzero(&msh, sizeof(msh));
 	if (dup_envp(envp, &msh))
 		return (EXIT_FAILURE);
-	printf("%s\n", get_env_var("PATH", 0, msh.envp));
 	str = ft_strdup("echo \"Hello'\"'\"World' '' $USER | cat -e >> my_outfile \
 | wc");
 	parse_cmds(str, &msh);
-	free(str);
+	destroy_msh(msh);
 	return (EXIT_SUCCESS);
 }
