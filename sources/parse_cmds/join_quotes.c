54cba08991abd5319573f1376f10789703a73b79
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-21 14:17:36 +0200

    Fix join_quotes

--- sources/parse_cmds/join_quotes.c
+++ sources/parse_cmds/join_quotes.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 17:00:39 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/16 17:43:26 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/21 14:17:07 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,23 @@ bool	join_quotes(char **strs)
 	size_t	i;
 
 	i = 0;
-	while (strs[++i])
+	while (strs[i])
 	{
-		if (ft_strchr(SEPARATORS, strs[i - 1][0])
-			|| ft_strchr(SEPARATORS, strs[i][0]))
+		if (!strs[i + 1] || ft_strchr(SEPARATORS, strs[i][0])
+			|| ft_strchr(SEPARATORS, strs[i + 1][0]))
+		{
+			i++;
 			continue ;
-		str = ft_strjoin(strs[i - 1], strs[i]);
+		}
+		str = ft_strjoin(strs[i], strs[i + 1]);
 		if (!str)
 		{
 			perror("malloc");
 			return (true);
 		}
-		shift_strs(&strs[i - 1]);
-		free(strs[i - 1]);
-		strs[i - 1] = str;
+		shift_strs(&strs[i]);
+		free(strs[i]);
+		strs[i] = str;
 	}
 	return (false);
 }
