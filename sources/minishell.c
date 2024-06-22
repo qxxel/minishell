29ba2a308d05d7b4d802770c7996a5c5cd162584
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-22 23:04:08 +0200

    Add debug_print_cmds

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,11 +6,70 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/20 16:29:30 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/22 23:03:29 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	debug_print_argv(char **argv)
+{
+	size_t	i;
+
+	if (!argv)
+	{
+		printf("\t.argv = NULL,\n");
+		return ;
+	}
+	printf("\t.argv = {");
+	i = 0;
+	while (argv[i])
+	{
+		if (i)
+			printf(", ");
+		printf("\"%s\"", argv[i++]);
+	}
+	printf("},\n");
+}
+
+static void	debug_print_redirects(t_cmd cmd)
+{
+	size_t	i;
+
+	printf("\t.n_redirects = %zu,\n", cmd.n_redirects);
+	if (!cmd.redirects)
+	{
+		printf("\t.redirects = NULL\n");
+		return ;
+	}
+	printf("\t.redirects = {\n");
+	i = 0;
+	while (i < cmd.n_redirects)
+	{
+		if (i)
+			printf(",\n");
+		printf("\t\t{\n\t\t\t.option = %d,\n\t\t\t.out = %d,\n\t\t\t\
+.option = %s\n\t\t}", cmd.redirects[i].option, cmd.redirects[i].out,
+			cmd.redirects[i].name);
+		i++;
+	}
+	printf("\n\t}\n");
+}
+
+static void	debug_print_cmds(t_msh msh)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < msh.n_cmds)
+	{
+		printf("msh.cmds[%zu] = {\n", i);
+		debug_print_argv(msh.cmds[i].argv);
+		debug_print_redirects(msh.cmds[i]);
+		printf("}\n\n");
+		i++;
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -25,6 +84,7 @@ int	main(int argc, char **argv, char **envp)
 	str = ft_strdup("echo \"Hello'\"'\"World' '' $USER | cat -e >> my_outfile \
 | wc");
 	parse_cmds(str, &msh);
+	debug_print_cmds(msh);
 	destroy_msh(msh);
 	return (EXIT_SUCCESS);
 }
