a1d485db8b22f1f4eb933c132191182d5e49242a
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-23 16:30:38 +0200

    Fix ft_echo

--- sources/builtins/ft_echo.c
+++ sources/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 14:00:42 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/23 15:59:23 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/23 16:28:50 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,26 @@ static bool	create_buffer(bool newline, size_t size, char **buffer)
 
 static bool	init_buffer(char **argv, bool newline, size_t size, char **buffer)
 {
+	bool	space;
 	size_t	len;
 
 	if (!argv[0])
 		return (create_buffer(newline, size, buffer));
+	space = false;
 	len = ft_strlen(argv[0]);
-	if (len == 2 && argv[0][0] == '-' && argv[0][1] == 'e')
+	if (len == 2 && argv[0][0] == '-' && argv[0][1] == 'n')
 	{
 		newline = 0;
 		len = 0;
 	}
-	if (init_buffer(&argv[1], newline, size + len, buffer))
+	else if (size)
+		space = true;
+	if (init_buffer(&argv[1], newline, size + len + space, buffer))
 		return (true);
 	while (len--)
-		(*buffer)[size + len] = argv[0][len];
+		(*buffer)[size + len + space] = argv[0][len];
+	if (space)
+		(*buffer)[size] = ' ';
 	return (false);
 }
 
