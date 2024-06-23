4721dcdb43adb6a8778ea3c17948afba46a3d469
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-23 13:06:55 +0200

    Use free_nptr instead of destroy_envp

--- sources/destroy_msh.c
+++ sources/destroy_msh.c
@@ -6,26 +6,14 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 16:26:49 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/22 22:00:26 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/23 13:06:19 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	destroy_envp(char **envp)
-{
-	size_t	i;
-
-	if (!envp)
-		return ;
-	i = -1;
-	while (envp[++i])
-		free(envp[i]);
-	free(envp);
-}
-
 void	destroy_msh(t_msh msh)
 {
-	destroy_envp(msh.envp);
+	free_nptr(2, msh.envp);
 	free_cmds(msh.cmds, msh.n_cmds);
 }
