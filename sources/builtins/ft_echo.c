5adb1ff70af1930d8ca715480bbad00fdc596c0d
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-23 15:52:20 +0200

    Add ft_echo

---
+++ sources/builtins/ft_echo.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/23 14:00:42 by deydoux           #+#    #+#             */
+/*   Updated: 2024/06/23 15:52:02 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "builtins.h"
+
+static bool	create_buffer(bool newline, size_t size, char **buffer)
+{
+	*buffer = malloc((size + newline + 1) * sizeof(char));
+	if (!*buffer)
+	{
+		perror("malloc");
+		return (true);
+	}
+	if (newline)
+		(*buffer)[size] = '\n';
+	(*buffer)[size + newline] = 0;
+	return (false);
+}
+
+static bool	init_buffer(char **argv, bool newline, size_t size, char **buffer)
+{
+	size_t	len;
+
+	if (!argv[0])
+		return (create_buffer(newline, size, buffer));
+	len = ft_strlen(argv[0]);
+	if (len == 2 && argv[0][0] == '-' && argv[0][1] == 'e')
+	{
+		newline = 0;
+		len = 0;
+	}
+	if (init_buffer(&argv[1], newline, size + len, buffer))
+		return (true);
+	while (len--)
+		buffer[size + len] = argv[0][len];
+	return (false);
+}
+
+int	ft_echo(char **argv, char **envp)
+{
+	bool	newline;
+	char	*buffer;
+	size_t	i;
+
+	if (init_buffer(&argv[1], true, 0, &buffer))
+		return (EXIT_FAILURE);
+	ft_putstr_fd(buffer, STDOUT_FILENO);
+	free(buffer);
+	return (EXIT_SUCCESS);
+	(void)envp;
+}
