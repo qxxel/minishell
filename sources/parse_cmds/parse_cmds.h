adc5b4445401a0657a8dd92f9deb676a095e2127
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-06-28 17:21:51 +0200

    Review parse_cmds error message

--- sources/parse_cmds/parse_cmds.h
+++ sources/parse_cmds/parse_cmds.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 16:33:52 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/28 15:51:20 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/28 17:21:10 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 
 # include "msh_commons.h"
 
-# define EXPECTED_CHAR_ERROR	"minishell: unexpected EOF while looking for \
-matching `%c'\n"
-# define EXPECTED_CHARS_ERROR	"minishell: unexpected EOF while looking for \
-matching `%c%c'\n"
-# define EXPECTED_STR_ERROR	"minishell: unexpected EOF while looking for \
-matching `%s'\n"
+# define EXPECTED_CHAR_ERROR	"minishell: syntax error near unexpected token \
+`%c'\n"
+# define EXPECTED_CHARS_ERROR	"minishell: syntax error near unexpected token \
+`%c%c'\n"
+# define EXPECTED_STR_ERROR		"minishell: syntax error near unexpected token \
+`%s'\n"
 # define QUOTES					"\"'"
 # define SEPARATORS				" \"'<|>"
-# define UNEXPECTED_EOF_ERROR	"minishell: syntax error: unexpected end of \
-file\n"
+# define UNEXPECTED_EOF_ERROR	"minishell: unexpected EOF while looking for \
+matching `%s'\nminishell: syntax error: unexpected end of file\n"
 
 bool	check_syntax(char **strs);
 bool	expand_env(char **str, char **envp);
