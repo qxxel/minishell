c5a1f3ed439b441af3aae34c253c75311e17a0d6
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-08 15:14:35 +0200

    Rename error to status

--- sources/parse_cmds/parse_cmds.c
+++ sources/parse_cmds/parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 16:38:50 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/08 12:40:53 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/08 15:14:05 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 bool	parse_cmds(char *str, t_msh *msh)
 {
-	bool	error;
+	bool	status;
 	char	**strs;
 
 	strs = NULL;
-	error = sign_expansion(str) || expand_env(&str, msh->envp)
+	status = sign_expansion(str) || expand_env(&str, msh->envp)
 		|| msh_split(str, &strs);
 	free(str);
-	error = error || expand_quotes(strs, msh->envp) || join_strs(strs)
+	status = status || expand_quotes(strs, msh->envp) || join_strs(strs)
 		|| check_syntax(strs) || init_cmds(strs, msh);
 	free_nptr(2, strs);
-	return (error);
+	return (status);
 }
