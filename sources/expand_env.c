a80db76330fed5cb1c1d313b3943e6676a1807e8
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-18 14:27:23 +0200

    Update expand_env

--- sources/expand_env.c
+++ sources/expand_env.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:16:11 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/08 11:08:13 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/18 14:26:52 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,47 +24,51 @@ static bool	init_expand(size_t size, char **expand)
 	return (false);
 }
 
-static size_t	expand_size(char *str, size_t *var_len, char **value,
+static size_t	expand_size(char *str, size_t *id_len, char **value,
 	char **envp)
 {
-	*var_len = 0;
-	if (str[0] == '$')
-	{
-		if (ft_isdigit(str[1]))
-			(*var_len)++;
-		else
-			while (str[*var_len + 1]
-				&& (ft_isalnum(str[*var_len + 1]) || str[*var_len + 1] == '_'))
-				(*var_len)++;
-		if (!*var_len)
-			return (1);
-		*value = get_env_var(str + 1, *var_len, envp);
-		if (!*value)
-			return (0);
-		return (ft_strlen(*value));
-	}
-	return (1);
+	*id_len = 0;
+	if (str[0] != '$')
+		return (1);
+	if (ft_isdigit(str[1]))
+		(*id_len)++;
+	else
+		while (str[*id_len + 1]
+			&& (ft_isalnum(str[*id_len + 1]) || str[*id_len + 1] == '_'))
+			(*id_len)++;
+	if (!*id_len)
+		return (1);
+	*value = get_env_var(str + 1, *id_len, envp);
+	if (!*value)
+		return (0);
+	return (ft_strlen(*value));
+}
+
+static char	sign_expand(char c)
+{
+	if (ft_strchr(EXPAND_SEPARATORS, c))
+		return (c * -1);
+	return (c);
 }
 
 static bool	create_expand(char *str, size_t size, char **envp, char **expand)
 {
 	char	*value;
-	size_t	value_size;
-	size_t	var_len;
+	size_t	id_len;
+	size_t	value_len;
 
 	if (!*str)
 		return (init_expand(size, expand));
-	value_size = expand_size(str, &var_len, &value, envp);
-	if (create_expand(str + var_len + 1, size + value_size, envp, expand))
+	value_len = expand_size(str, &id_len, &value, envp);
+	if (create_expand(&str[id_len + 1], size + value_len, envp, expand))
 		return (true);
 	if (str[0] == '$')
 	{
-		if (!var_len)
+		if (!id_len)
 			(*expand)[size] = '$';
 		else if (value)
-			while (value_size--)
-				(*expand)[size + value_size] = value[value_size]
-					* (-1 + (2 * (value[value_size] == ' ')));
+			while (value_len--)
+				(*expand)[size + value_len] = sign_expand(value[value_len]);
 	}
 	else
 		(*expand)[size] = *str;
