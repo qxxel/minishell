95ae94dd2ded0e12f8e95cf293f16c17319fd746
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-22 18:35:53 +0200

    Use safe_atoll instead of ft_atoi for exit builtin

--- sources/builtins/ft_exit.c
+++ sources/builtins/ft_exit.c
@@ -6,30 +6,42 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 16:04:15 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/10 15:03:05 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/22 18:37:21 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-static bool	str_is_digits(char *str)
+static bool	safe_atoll(char *str, long long *n)
 {
-	size_t	i;
+	char		sign;
+	long long	tmp_n;
 
-	i = str[0] == '+' || str[0] == '-';
-	while (ft_isdigit(str[i]))
-		i++;
-	return (!str[i]);
+	sign = 1;
+	tmp_n = 0;
+	*n = 0;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		sign -= 2 * (*str++ == '-');
+	while (ft_isdigit(*str))
+	{
+		tmp_n = tmp_n * 10 + (*str++ - '0') * sign;
+		if (*n != tmp_n / 10)
+			return (true);
+		*n = tmp_n;
+	}
+	return (*str != 0);
 }
 
 int	ft_exit(char **argv, t_msh *msh)
 {
-	int	status;
+	long long	status;
 
 	ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (!argv[0] || !argv[1])
 		status = EXIT_SUCCESS;
-	else if (!str_is_digits(argv[1]))
+	else if (safe_atoll(argv[1], &status))
 	{
 		ft_dprintf(STDERR_FILENO, EXIT_ARG_ERROR, argv[1]);
 		status = 2;
@@ -39,8 +51,6 @@ int	ft_exit(char **argv, t_msh *msh)
 		ft_putstr_fd(EXIT_ARGS_ERROR, STDERR_FILENO);
 		return (1);
 	}
-	else
-		status = ft_atoi(argv[1]);
 	free_cmds(msh->cmds, msh->n_cmds);
 	destroy_msh(*msh);
 	exit(status);
