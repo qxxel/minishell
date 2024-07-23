fe6d773061641b860211a88ecb75e513c49ce6e2
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-23 17:12:42 +0200

    Fix g_status assignation on parse_cmds error

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/23 15:06:42 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/23 17:10:00 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ int	main(int argc, char **argv, char **envp)
 			break ;
 		}
 		add_history(str);
-		parse_cmds(str, &msh);
-		status = exec_cmds(&msh);
+		if (!parse_cmds(str, &msh))
+			status = exec_cmds(&msh);
 		free_cmds(msh.cmds, msh.n_cmds);
 		msh.cmds = NULL;
 		msh.n_cmds = 0;
