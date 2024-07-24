baf300aa482baeaddf12e839ddded547bceb0c62
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-24 14:18:43 +0200

    Add readline_prompt

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,13 +6,41 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:04:18 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/23 17:10:00 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/24 14:17:24 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 int	g_status = 0;
+
+static char	*generate_prompt(t_msh msh)
+{
+	char	*path;
+	char	*prompt;
+
+	if (msh.pwd)
+		path = msh.pwd;
+	else
+		path = get_env_var("PWD", 3, msh.envp);
+	if (!path)
+		path = ".";
+	prompt = ft_strjoin(path, "$ ");
+	return (prompt);
+}
+
+static char	*readline_prompt(t_msh msh)
+{
+	char	*prompt;
+	char	*str;
+
+	prompt = generate_prompt(msh);
+	if (!prompt)
+		return (readline("$ "));
+	str = readline(prompt);
+	free(prompt);
+	return (str);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -23,7 +51,7 @@ int	main(int argc, char **argv, char **envp)
 	status = init_msh(envp, &msh);
 	while (!status)
 	{
-		str = readline("$ ");
+		str = readline_prompt(msh);
 		if (!str)
 		{
 			ft_putstr_fd("exit\n", STDERR_FILENO);
