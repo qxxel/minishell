1d971b79ef907bfc4aed532b353c66b6e0fb5068
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-07-25 15:57:15 +0200

    Print SIGQUIT message

--- sources/minishell.h
+++ sources/minishell.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:05:57 by deydoux           #+#    #+#             */
-/*   Updated: 2024/07/25 15:46:56 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/25 15:52:35 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # define EXPAND_SEPARATORS		"<|>"
 # define INT_TO_STR_MAX_SIZE	12
 # define INT_TO_STR_MAX_SIZE	12
-# define SIGINT_CODE            130
-# define SIGQUIT_MESSAGE	    "Quit (core dumped)\n"
+# define SIGINT_CODE			130
+# define SIGQUIT_MESSAGE		"Quit (core dumped)\n"
 
 bool	exec_cmds(t_msh *msh);
 bool	init_msh(char **envp, t_msh *msh);
--- sources/set_sig_exec.c
+++ sources/set_sig_exec.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 17:09:21 by agerbaud          #+#    #+#             */
-/*   Updated: 2024/07/25 15:49:07 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/07/25 15:55:37 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	handle_sigint(int sig)
 	(void)sig;
 }
 
-void	set_sig_exec(void)
+static void	set_sigint(void)
 {
 	struct sigaction	act;
 
@@ -29,4 +29,28 @@ void	set_sig_exec(void)
 	sigemptyset(&act.sa_mask);
 	act.sa_handler = &handle_sigint;
 	sigaction(SIGINT, &act, NULL);
+}
+
+static void	handle_sigquit(int sig)
+{
+	while (wait(NULL) != -1)
+		;
+	ft_putstr_fd(SIGQUIT_MESSAGE, STDERR_FILENO);
+	(void)sig;
+}
+
+static void	set_sigquit(void)
+{
+	struct sigaction	act;
+
+	ft_bzero(&act, sizeof(act));
+	sigemptyset(&act.sa_mask);
+	act.sa_handler = &handle_sigquit;
+	sigaction(SIGQUIT, &act, NULL);
+}
+
+void	set_sig_exec(void)
+{
+	set_sigint();
+	set_sigquit();
 }
