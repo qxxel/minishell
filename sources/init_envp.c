bb50c066eb69d95b3ecda59ccff69f9d0d8a7a59
agerbaud <agerbaud@student.42lyon.fr>
2024-08-01 11:52:58 +0200

    fix init_envp memory leak

--- sources/init_envp.c
+++ sources/init_envp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_envp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: agerbaud <agerbaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 15:48:38 by deydoux           #+#    #+#             */
-/*   Updated: 2024/06/28 15:49:05 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/08/01 11:52:07 by agerbaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ bool	init_envp(char **src, char ***envp)
 		if (!(*envp)[i])
 		{
 			perror("malloc");
+			free_nptr(2, *envp);
 			return (true);
 		}
 		i++;
